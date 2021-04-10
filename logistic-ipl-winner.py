afcb69aca609aa83e1f99401c5fd5987864b4b75
Harsh Mange <harshmange44@gmail.com>
2021-04-11 02:05:29 +0530

    improoved, bugs fixed & new dataset

--- logistic-ipl-winner.py
+++ logistic-ipl-winner.py
@@ -1,101 +1,94 @@
+# IMPORTING libraries
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+# sigmoid function: provides hypothesis & prediction
+# Params: X - Input data with feature vars. & theta: weight
 def sigmoid_func(X, theta):
     z = np.dot(X, theta)
     return 1 / (1 + np.exp(-z))
 
+# gradient descent function: provides optimization in Logistic Regression by minimizing cost function
+# Params: X - training data with feature vars. , y: training data with target var & h: val of sigmoid func. 
 def gradient_descent(X, h, y):
     return np.dot(X.T, (h - y))/y.shape[0]
 
+# update theta function: update theta to best suitable theta
+# Params: theta: weight, alpha: learning rate & gradient: val of gradient descent func.
 def update_theta(theta, alpha, gradient):
     return theta - alpha*gradient
 
-dataFrame = pd.read_csv("dataset_files/finalest_ipl1.csv") # dataset
+# train the model
+def train(itr, l_rate, theta, x_train, y_train):
+    optimized_thetas = [] # list of best suitable thetas at diff learning rate
 
-seasonMapping = {2008: 1, 2009: 2, 2010: 3, 2011: 4, 2012: 5, 2013: 6, 2014: 7, 2015: 8, 2016: 9, 2017: 10, 2018: 11, 2019: 12, 2020: 13}
-dataFrame["season"] = dataFrame["season"].map(seasonMapping)
+    for rate in l_rate:
+        for i in range(itr):
+            y1 = sigmoid_func(x_train, theta)
+            gradient = gradient_descent(x_train, y1, y_train)
+            theta = update_theta(theta, rate, gradient)
 
-team1Mapping = {"CSK": 2, "DC": 3, "RCB": 4, "RR": 5, "MI": 6, "KKR": 7, "KXIP": 8, "SRH": 9}
-dataFrame["team1"] = dataFrame["team1"].map(team1Mapping)
+        optimized_thetas.append(theta)
 
-team2Mapping = {"CSK": 2, "DC": 3, "RCB": 4, "RR": 5, "MI": 6, "KKR": 7, "KXIP": 8, "SRH": 9}
-dataFrame["team2"] = dataFrame["team2"].map(team2Mapping)
+    return optimized_thetas
 
-toss_winnerMapping = {"CSK": 2, "DC": 3, "RCB": 4, "RR": 5, "MI": 6, "KKR": 7, "KXIP": 8, "SRH": 9}
-dataFrame["toss_winner"] = dataFrame["toss_winner"].map(toss_winnerMapping)
+# predict the data using trained model
+def predict(itr, l_rate, thetas, x_test, y_test):
+    index = 0
+    print("------------------")
+    print("Iterations:", itr)
+    print("------------------")
+    for theta in thetas:
+        pred_y = sigmoid_func(x_test, theta) # predicting 'x_test' data using theta 
 
-toss_decisionMapping = {"bat": 1, "field": 2, "bowl": 2}
-dataFrame["toss_decision"] = dataFrame["toss_decision"].map(toss_decisionMapping)
+        # converting large decimal values to .2 & forming data frame
+        pred_y_round_off = pd.DataFrame(np.around(pred_y, decimals=2))
 
-winnerMapping = {"CSK": 2, "DC": 3, "RCB": 4, "RR": 5, "MI": 6, "KKR": 7, "KXIP": 8, "SRH": 9}
-dataFrame["winner"] = dataFrame["winner"].map(winnerMapping)
+        # classifying predicted vals in 0 & 1 based on threshold value: 0.5
+        pred_y_round_off.loc[pred_y_round_off[0] < 0.5, "predicted"] = 0
+        pred_y_round_off.loc[pred_y_round_off[0] >= 0.5, "predicted"] = 1
+        pred_y_round_off['winner'] = pd.DataFrame(y_test)
 
-dataFrame.loc[dataFrame["winner"]==dataFrame["team1"],"winner"]=1
-dataFrame.loc[dataFrame["winner"]!=dataFrame["team1"],"winner"]=0
+        # calculating accuracy by comparing actual & predicted vals
+        accuracy = (pred_y_round_off.loc[pred_y_round_off['predicted'] == pred_y_round_off['winner']].shape[0]/pred_y_round_off.shape[0])*100
+        
+        print("Learning rate:", l_rate[index])
+        index += 1
+        print("Accuracy: ", accuracy)
+        print("\n")
 
-dataFrame.loc[dataFrame["toss_winner"]==dataFrame["team1"],"toss_winner"]=1
-dataFrame.loc[dataFrame["toss_winner"]!=dataFrame["team1"],"toss_winner"]=0
+# loading dataset
+dataFrame = pd.read_csv("dataset_files/encoded_dataset.csv")  # dataset
+
+# mapping
+dataFrame.loc[dataFrame["teampoint_diff"] < 0, "teampoint_diff"] = 0
+dataFrame.loc[dataFrame["teampoint_diff"] >= 0, "teampoint_diff"] = 1
 
 df = dataFrame.copy()
 
-feature_names = ['season', 'team1', 'team2', 'toss_winner', 'toss_decision', 'team1_point', 'team1_home_advantage']
+feature_vars = ['team1', 'powerplay_runs_team1', 'powerplay_wickets_team1', 'team2', 'powerplay_runs_team2',
+                 'powerplay_wickets_team2', 'homeground_advantage', 'team1_toss_win', 'teampoint_diff']
 
-X = df[feature_names]
-y = df['winner']
+target_var = 'team1_win'
 
+X_features = df[feature_vars]
+y_target = df[target_var]
 
-learning_rate = 0.1
-iterations = 200
+learning_rate = [0.1, 0.5, 1] #0.0001, 0.001, 0.01, 
+iterations = 5000
 
-# print(X.to_string())
+intercept = np.ones((X_features.shape[0], 1))
+X_features = np.concatenate((intercept, X_features), axis=1)
 
-# x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0 )
+# splitting the data
+X_train, X_test, Y_train, Y_test = train_test_split(X_features, y_target, test_size=1/3, random_state=0)
+Y_train = Y_train.to_numpy() # converting series to array
+Y_test = Y_test.to_numpy() # converting series to array
 
-# x_train = pd.DataFrame([x_train])
-# y_train = pd.DataFrame([y_train])
-# x_test = pd.DataFrame([x_test])
-# y_test = pd.DataFrame([y_test])
+theta = np.zeros(X_train.shape[1]) # init. theta
 
-intercept = np.ones((X.shape[0], 1))
-
-# print(intercept)
-
-X = np.concatenate((intercept, X), axis=1)
-# print(X)
-
-theta = np.zeros(X.shape[1])
-# theta = np.random.rand(X.shape[1],1)
-# print(theta)
-
-for i in range(iterations):
-    y1 = sigmoid_func(X, theta)
-    gradient = gradient_descent(X, y1, y)
-    # print("gradient",gradient)
-    theta = update_theta(theta, learning_rate, gradient)
-    
-print("Learning rate:",learning_rate)
-print("Iterations:",iterations)
-
-pred_y = sigmoid_func(X, theta)
-
-pred_y_round_off = pd.DataFrame(np.around(pred_y, decimals=2))
-pred_y_round_off.loc[pred_y_round_off[0] >= 0.5,"predicted"]=1
-pred_y_round_off.loc[pred_y_round_off[0] < 0.5,"predicted"]=0
-
-
-# print(pred_y_round_off['predicted'].to_string())
-
-# count = 0
-# for count in range( np.size( pred_y_round_off ) ) :
-		
-# 	if y_test[count] == pred_y_round_off[count] :			
-# 		correctly_classified = correctly_classified + 1
-			
-# 	count = count + 1
-		
-# print( "Accuracy:", (correctly_classified / count ) * 100 )
-
-accuracy = (pred_y_round_off.loc[pred_y_round_off['predicted']==y[0]].shape[0]/pred_y_round_off.shape[0])*100
-print("Accuracy: ", accuracy)
+# training the model
+model = train(iterations, learning_rate, theta, X_train, Y_train)
+# predicting
+predict(iterations, learning_rate, model, X_test, Y_test)
